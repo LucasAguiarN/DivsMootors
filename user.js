05ba80a2393b08c715eed638164d8ceefe6a9b3c
Lucas-Aguiar-Nunes <lucas_aguiar_nunes@outlook.com>
2025-04-05 17:18:45 -0300

    melhorando logica

--- user.js
+++ user.js
@@ -1,24 +1,18 @@
-function validar_dados() {
-    let dados = [];
+function validar_dados(){
     let name = document.getElementById("nome").value;
-    dados.push(name);
     let email = document.getElementById("email").value;
-    dados.push(email);
     let cpf_cnpj = document.getElementById("cpf_cnpj").value;
-    dados.push(cpf_cnpj);
     let birthday = document.getElementById("birthday").value;
-    dados.push(birthday);
     let password = document.getElementById("password").value;
-    dados.push(password);
 
-    if (!terms.checked){
-        alert("Aceite os Termos de Condição");
-    }
-    else if (dados.includes("")){
+    if (!name || !email || !cpf_cnpj || !birthday || !password){
         alert("Preencha Todos os Campos");
     }
     else if (password.length < 6){
         alert("Senha deve Conter no Minimo 6 Digitos!");
+    }
+    else if (!terms.checked){
+        alert("Aceite os Termos de Condição");
     }
     else {
         enviar_cadastro(name, email, cpf_cnpj, birthday, password);
@@ -39,32 +33,29 @@ async function enviar_cadastro(name, email, cpf_cnpj, birthday, password){
         "birthday": birthday
     }
 
-    let api = await fetch(                              //Conectar API
+    let api = await fetch(                              
         "https://go-wash-api.onrender.com/api/user",{
             method:"POST",
-            body:JSON.stringify(dados),                 //JSON.stringify Função do JS q converte dados q vão ser enviados para JSON
+            body:JSON.stringify(dados),                
             headers:{
-                'Content-Type':'application/json'       //Cabeçalho q informa q conteudo de conexão é JSON
+                'Content-Type':'application/json'
             }                    
         }
     );
     
-    let resposta = await api.json();                    //Variavel recebe resposta da API como um objeto em JSON
-    console.log(resposta);                              //Exibe no Console para Fins de Depuração
+    let resposta = await api.json();                    
+    console.log(resposta);                              
 
     if (api.ok){
-        alert(resposta.data)
-        document.getElementById("form").reset();
+        alert("Enviamos um link de ativação de conta no email:\n"+email+" -> clique e ative sua conta");
+        form.reset();
     }
-    else{                                               //Se não tiver sucesso vai ter recebido o erro como um objeto
+    else{                                               
         if (resposta.data.errors.cpf_cnpj){
             alert("CPF ou CNPJ Já Cadastrado");
         }
         if (resposta.data.errors.email){                
-            alert("Email Já Cadastrado!")
+            alert("Email Já Cadastrado!");
         }
     }
 }
-
-//https://www.invertexto.com/gerador-email-temporario
-//https://www.4devs.com.br/gerador_de_cpf
