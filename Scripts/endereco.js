504201a3f18e32615035dae7fbd69c1b2aabfe8e
Lucas-Aguiar-Nunes <lucas_aguiar_nunes@outlook.com>
2025-05-18 16:57:36 -0300

    atualizar cadastro e preenchimento

--- Scripts/endereco.js
+++ Scripts/endereco.js
@@ -2,14 +2,14 @@ let token = localStorage.getItem('access_token');
 let nome = document.querySelector(".hero-content h1");
 nome.innerText = "Olá "+localStorage.getItem('nome');
 
-function exibir_form(botao){
+async function exibir_form(botao){
     let title = document.querySelector("#form h1");
     if (botao.id == "cadastrar"){
-        
         title.innerText = "Cadastrar";
     }
     else if (botao.id == "atualizar"){
         title.innerText = "Atualizar";
+        await preencher();
     }
     
     if (botao.id != "cancelar"){
@@ -24,7 +24,7 @@ function exibir_form(botao){
     }
 }
 
-function validar_dados(botao){
+function validar_dados(botao){  
     let nome = document.getElementById("nome").value;
     let cep = document.getElementById("cep").value;
     let endereco = document.getElementById("endereco").value;
@@ -83,10 +83,27 @@ async function listar(){
         if (!request.ok){
             throw new Error("Erro!\nStatus "+response.status);
         }
+        return resposta
     }
     catch(error){
         console.log(error);
     }
+}
+
+async function preencher() {
+    let nome = document.getElementById("nome");
+    let cep = document.getElementById("cep");
+    let endereco = document.getElementById("endereco");
+    let numero = document.getElementById("numero");
+    let complemento = document.getElementById("complemento");
+
+    let lista = await listar();
+    // pelo id
+    nome.value = lista.data[0].title
+    cep.value = lista.data[0].cep
+    endereco.value = lista.data[0].address
+    numero.value = lista.data[0].number
+    complemento.value = lista.data[0].complement
 }
 
 async function enviar_endereco(dados) {
@@ -117,10 +134,10 @@ async function enviar_endereco(dados) {
 }
 
 async function atualizar_endereco(dados) {
-    // Rascunho
+    //Rascunho
     try{
         let request = await fetch(                              
-            "https://go-wash-api.onrender.com/api/auth/address{ID}",{ // id do endereço (listagem [i].id)
+            "https://go-wash-api.onrender.com/api/auth/address/33",{
                 method:"POST",
                 body:JSON.stringify(dados),                
                 headers:{
@@ -145,10 +162,10 @@ async function atualizar_endereco(dados) {
 }
 
 async function excluir_endereco() {
-    // Rascunho    
+    // Rascunho   
     try{
         let request = await fetch(
-            "https://go-wash-api.onrender.com/api/auth/address{ID}",{
+            "https://go-wash-api.onrender.com/api/auth/address/{ID}",{
             method:"DELETE",          
             headers:{
                 "Authorization": `Bearer ${token}`
