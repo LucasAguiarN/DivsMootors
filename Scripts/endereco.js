1d0707395ebe0c17c994bd8885f863d710cbcab8
Lucas-Aguiar-Nunes <lucas_aguiar_nunes@outlook.com>
2025-05-31 23:10:39 -0300

    backend de listar, atualizar e deletar

--- Scripts/endereco.js
+++ Scripts/endereco.js
@@ -1,30 +1,27 @@
 let token = localStorage.getItem('access_token');
 let nome = document.querySelector(".hero-content h1");
 nome.innerText = "Olá "+localStorage.getItem('nome');
+let lista_enderecos = []
 
 async function exibir_form(botao){
     let title = document.querySelector("#form h1");
-    if (botao.id == "cadastrar"){
+    document.getElementById("lista").style.display = 'none';
+    if (botao){
         title.innerText = "Cadastrar";
-    }
-    else if (botao.id == "atualizar"){
-        title.innerText = "Atualizar";
-        await preencher();
-    }
-    
-    if (botao.id != "cancelar"){
-        document.getElementById("form").style.display = 'block';
-        document.getElementById("salvar").addEventListener("click", function () {
-            validar_dados(botao);
-        });
-    }
-    else{
         form.reset();
-        document.getElementById("form").style.display = 'none';
     }
+    else {
+        title.innerText = "Atualizar";
+    }
+    document.getElementById("form").style.display = 'block';
 }
 
-function validar_dados(botao){  
+function cancela(){
+    form.reset();
+    document.getElementById("form").style.display = 'none';
+}
+
+function validar_dados(){  
     let nome = document.getElementById("nome").value;
     let cep = document.getElementById("cep").value;
     let endereco = document.getElementById("endereco").value;
@@ -42,7 +39,8 @@ function validar_dados(botao){
         "number": numero,
         "complement": complemento,
     }
-    if (botao.id == "cadastrar"){
+    let title = document.querySelector("#form h1");
+    if (title.innerText == "Cadastrar"){
         enviar_endereco(dados);
     }
     else{
@@ -68,6 +66,8 @@ async function buscar_endereco() {
 }
 
 async function listar(){
+    document.getElementById("lista").style.display = 'block';
+    document.getElementById("form").style.display = 'none';
     try{
         let request = await fetch(
             "https://go-wash-api.onrender.com/api/auth/address",{
@@ -83,27 +83,46 @@ async function listar(){
         if (!request.ok){
             throw new Error("Erro!\nStatus "+response.status);
         }
-        return resposta
+        lista_enderecos = resposta.data;
+        let tabelaEnderecos = document.getElementById("lista_enderecos");
+        tabelaEnderecos.innerHTML = "";  
+        lista_enderecos.forEach((endereco) => {
+        let row = `<tr>
+            <td>${endereco.title}</td>
+            <td>${endereco.cep}</td>
+            <td>${endereco.address}</td>
+            <td>${endereco.number}</td>
+            <td>${endereco.complement || ""}</td>
+            <td><button onclick="preencher(${endereco.id})">Atualizar</button></td>
+            <td><button onclick="excluir_endereco(${endereco.id})">Remover</button></td>
+        </tr>`;
+        tabelaEnderecos.innerHTML += row;
+    });
     }
     catch(error){
         console.log(error);
     }
 }
 
-async function preencher() {
+async function preencher(id) {
     let nome = document.getElementById("nome");
     let cep = document.getElementById("cep");
-    let endereco = document.getElementById("endereco");
+    let morada = document.getElementById("endereco");
     let numero = document.getElementById("numero");
     let complemento = document.getElementById("complemento");
-
-    let lista = await listar();
-    // pelo id
-    nome.value = lista.data[0].title
-    cep.value = lista.data[0].cep
-    endereco.value = lista.data[0].address
-    numero.value = lista.data[0].number
-    complemento.value = lista.data[0].complement
+    
+    for (let index = 0; index < lista_enderecos.length; index++){
+        if (lista_enderecos[index].id == id){
+            nome.value = lista_enderecos[index].title;
+            cep.value = lista_enderecos[index].cep;
+            morada.value = lista_enderecos[index].address;
+            numero.value = lista_enderecos[index].number;
+            complemento.value = lista_enderecos[index].complement;
+        }
+    }
+    let id_endereco = document.getElementsByClassName("endereco")[0];
+    id_endereco.id = id
+    exibir_form();
 }
 
 async function enviar_endereco(dados) {
@@ -121,8 +140,8 @@ async function enviar_endereco(dados) {
 
         let resposta = await request.json();
         console.log(resposta);
-        document.getElementById("form").style.display = 'none';
         alert("Endereço Cadastrado com Sucesso!");
+        cancela()
 
         if (!request.ok){
             throw new Error("Erro!\nStatus "+response.status);
@@ -134,10 +153,10 @@ async function enviar_endereco(dados) {
 }
 
 async function atualizar_endereco(dados) {
-    //Rascunho
+    let id = document.getElementsByClassName("endereco")[0].id;
     try{
         let request = await fetch(                              
-            "https://go-wash-api.onrender.com/api/auth/address/33",{
+            "https://go-wash-api.onrender.com/api/auth/address/"+id,{
                 method:"POST",
                 body:JSON.stringify(dados),                
                 headers:{
@@ -161,11 +180,10 @@ async function atualizar_endereco(dados) {
     }
 }
 
-async function excluir_endereco() {
-    // Rascunho   
+async function excluir_endereco(id) {
     try{
         let request = await fetch(
-            "https://go-wash-api.onrender.com/api/auth/address/{ID}",{
+            "https://go-wash-api.onrender.com/api/auth/address/"+id,{
             method:"DELETE",          
             headers:{
                 "Authorization": `Bearer ${token}`
@@ -178,6 +196,8 @@ async function excluir_endereco() {
         if (!request.ok){
             throw new Error("Erro!\nStatus "+response.status);
         }
+        alert("Endereço Excluído com Sucesso!");
+        listar()
     }
     catch(error){
         console.log(error);
